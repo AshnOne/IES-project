57bc4ffe8007c650a3ca3c578304c978b7b7a981
ViniiNoites <vinicius.dias2@unioeste.br>
2022-11-10 23:31:29 -0300

    Exercícios resolvidos

--- Exercicios/Grupo 7 (Ponteiros e Structs)/exec.1.c
+++ Exercicios/Grupo 7 (Ponteiros e Structs)/exec.1.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-void operation(float *soma, float *dif, float *prod, float a, float b){
+void SomaDiferencaProduto(float *soma, float *dif, float *prod, float a, float b)
+{ // FUNCAO QUE RECEBE COMO PARAMENTRO A SOMA, DIF E PROD DE a E b
     *soma = a + b;
     *dif = a - b;
     *prod = a * b;
 }
-int main(){
+int main()
+{
     float a, b, soma, dif, prod;
 
     printf("digite dois numeros:\n");
     scanf("%f %f", &a, &b);
 
-    operation(&soma, &dif, &prod, a, b);
+    SomaDiferencaProduto(&soma, &dif, &prod, a, b); // CHAMADA DA FUNCAO Q RETORNA AS OPERACOESA
     printf("soma -> %0.3f + %0.3f = %0.3f\n", a, b, soma);
     printf("diferenca -> %0.3f - %0.3f = %0.3f\n", a, b, dif);
     printf("produto -> %0.3f * %0.3f = %0.3f\n", a, b, prod);
